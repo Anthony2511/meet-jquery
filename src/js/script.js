b856d90899fd387ef5356b474474a4517c1a4e99
Anthony2511 <anthonybeaumecker@gmail.com>
2017-02-13 14:42:15 +0100

    Exercice one

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+/* Anthony/meet-jquery
+*
+* /src/js/script.js - Main Script
+*
+* 13/02/2017
+*/
+
+window.addEventListener( "load", function() {
+
+    // 1. a with rel=external opens in new window
+     Array.from( document.querySelectorAll( 'a[rel*="external"]' ) ).forEach( function( $elt ) {
+         $elt.setAttribute( "target", "_new" );
+    } );
+    
+} );
