df5053ca50a8ffd64f2ecc8a6459391659342b20
Anthony2511 <anthonybeaumecker@gmail.com>
2017-02-13 16:38:48 +0100

    Exercice two

--- src/js/script.js
+++ src/js/script.js
@@ -5,9 +5,25 @@
 * 13/02/2017
 */
 
+const fHandleTab = function( oEvent ) {
+    let $this = $( this ); // element sur lequel a lieu l'évenement
+
+    oEvent.preventDefault();
+    if ( $this.parent().hasClass( "active" ) ) {
+        return;
+    }//element sur lequel on se trouve pas de class active
+    $( "ul.nav.nav-tabs .active" ).removeClass( "active" );//recup nav tabs active
+    $this.parent().addClass( "active" );
+    $( ".tab-pane.active" ).removeClass( "active" );//selectionner tous les ele tab pane
+    $( '#${ $this.data( "tab-target" ) }' ).addClass( "active" );
+};
+
 $( function() {
 
     // 1. a with rel=external
     $( 'a[rel*="external"]' ).attr( "target", "_new" ); // recup tous les éléments rel=external
-    //changer attribut target
+
+    // 2. tabs
+    $( "ul.nav.nav-tabs a" ).on( "click", fHandleTab ); // recup onglets
+
 } );
