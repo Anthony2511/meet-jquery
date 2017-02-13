b41d44065cd4ae9be3bbdfe2621418c8b0a8349a
Anthony2511 <anthonybeaumecker@gmail.com>
2017-02-13 16:22:37 +0100

    Exercice one

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+/*
+*
+* src/js/script.js
+*
+* 13/02/2017
+*/
+
+$(function () {
+
+    // 1. a with rel=external
+    $('a[rel*="external"]').attr("target", "_new"); // recup tous les éléments rel=external
+    //changer attribut target
+});
